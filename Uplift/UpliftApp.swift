f05c8040220de83456f4668dee215862690658d3
Vin Bui <vdb23@cornell.edu>
2023-11-25 14:00:46 -0500

    Install Nuke + SwiftLint

---
+++ Uplift/UpliftApp.swift
@@ -0,0 +1,32 @@
+//
+//  UpliftApp.swift
+//  Uplift
+//
+//  Created by Vin Bui on 11/25/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct UpliftApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
