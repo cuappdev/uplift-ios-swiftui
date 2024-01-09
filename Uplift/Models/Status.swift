0ac15f062c71fc337c52cf8f110edf7549ae5004
Vin Bui <vdb23@cornell.edu>
2024-01-09 12:21:42 -0600

    Gym sorting + fixed filter button calculation

--- Uplift/Models/Status.swift
+++ Uplift/Models/Status.swift
@@ -9,12 +9,34 @@
 import Foundation
 
 /// The status of the Gym or Facility.
-enum Status: Hashable {
+enum Status: Hashable, Comparable {
 
     /// Currently closed where `openTime` is the `Date` in which it will open next.
     case closed(openTime: Date)
 
     /// Currently open where `closeTime` is the `Date` in which it will begin closing.
     case open(closeTime: Date)
+
+    /// Open status is less than closed status.
+    static func < (lhs: Status, rhs: Status) -> Bool {
+        var lhsVal: Int
+        var rhsVal: Int
+
+        switch lhs {
+        case .closed:
+            lhsVal = 1
+        case .open:
+            lhsVal = 0
+        }
+
+        switch rhs {
+        case .closed:
+            rhsVal = 1
+        case .open:
+            rhsVal = 0
+        }
+
+        return lhsVal < rhsVal
+    }
 
 }
