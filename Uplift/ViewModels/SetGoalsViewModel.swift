2e9761857e89661f2e776f52c620346d3e1de883
Caitlyn Jin <cj384@cornell.edu>
2024-10-22 20:22:06 -0400

    Progress on setting goals screen
    - Slider for goal setting
    - Partial implementation of workout reminders

---
+++ Uplift/ViewModels/SetGoalsViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  SetGoalsViewModel.swift
+//  Uplift
+//
+//  Created by Caitlyn Jin on 10/22/24.
+//  Copyright © 2024 Cornell AppDev. All rights reserved.
+//
+
+import SwiftUI
+
+extension SetGoalsView {
+
+    /// The ViewModel for the Set Goals page view.
+    @MainActor
+    class ViewModel: ObservableObject {
+
+        // MARK: - Properties
+
+        @Published var selectedDays: [DayOfWeek] = []
+
+    }
+}
---
+++ Uplift/Views/Profile/SetGoalsView.swift
@@ -0,0 +1,295 @@
+//
+//  SetGoalsView.swift
+//  Uplift
+//
+//  Created by Caitlyn Jin on 10/22/24.
+//  Copyright © 2024 Cornell AppDev. All rights reserved.
+//
+
+import SwiftUI
+
+/// The view for setting goals and workout reminders.
+struct SetGoalsView: View {
+
+    // MARK: - Properties
+
+    @StateObject private var viewModel = ViewModel()
+    @Environment(\.dismiss) private var dismiss
+    @State private var isEveryDay = false
+    @State private var isSettingTime = false
+    @State private var showNewReminder = false
+
+    // MARK: - UI
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                header
+                content
+            }
+            .ignoresSafeArea(.all, edges: .top)
+            .navigationBarBackButtonHidden(true)
+            .toolbarBackground(.hidden, for: .navigationBar)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    NavBackButton(color: Constants.Colors.black, dismiss: dismiss)
+                }
+            }
+            .background(Constants.Colors.white)
+        }
+    }
+
+    private var header: some View {
+        VStack {
+            Spacer()
+
+            HStack {
+                Spacer()
+
+                Text("Goals")
+                    .foregroundStyle(Constants.Colors.black)
+                    .font(Constants.Fonts.h2)
+
+                Spacer()
+            }
+        }
+        .padding(.bottom, 8)
+        .background(Constants.Colors.lightGray)
+        .frame(height: 96)
+    }
+
+    private var content: some View {
+        VStack(spacing: 48) {
+            workoutDays
+            workoutReminders
+
+            Spacer()
+        }
+        .padding(
+            EdgeInsets(
+                top: Constants.Padding.goalsVertical,
+                leading: Constants.Padding.goalsHorizontal,
+                bottom: Constants.Padding.goalsVertical,
+                trailing: Constants.Padding.goalsHorizontal
+            )
+        )
+    }
+
+    private var workoutDays: some View {
+        VStack(spacing: 20) {
+            HStack {
+                Text("Let’s set a plan! ")
+                    .font(Constants.Fonts.h2)
+                +
+                Text("How many days a week would you like to work out?")
+                    .font(Constants.Fonts.f2)
+
+                Spacer()
+            }
+            .foregroundStyle(Constants.Colors.black)
+
+            VStack(spacing: 16) {
+                Slider(
+                    value: $capacity,
+                    in: 1...7,
+                    step: 1
+                )
+                .tint(Constants.Colors.yellow)
+                .frame(height: 8)
+
+                HStack {
+                    ForEach(1...7, id: \.self) { day in
+                        Text("\(day)")
+
+                        day < 7 ? Spacer() : nil
+                    }
+                }
+                .foregroundStyle(Constants.Colors.black)
+                .font(Constants.Fonts.bodyBold)
+                .padding(.horizontal, 10)
+            }
+        }
+    }
+
+    private var workoutReminders: some View {
+        VStack(spacing: 24) {
+            VStack(spacing: 12) {
+                HStack {
+                    Text("WORKOUT REMINDERS")
+                        .foregroundStyle(Constants.Colors.black)
+                        .font(Constants.Fonts.h2)
+
+                    Spacer()
+                }
+
+                HStack {
+                    Text("Get reminders on workout days to stay on track!")
+                        .foregroundStyle(Constants.Colors.gray04)
+                        .font(Constants.Fonts.f3)
+
+                    Spacer()
+                }
+            }
+
+            newReminder
+        }
+    }
+
+    private var newReminder: some View {
+        VStack(spacing: 16) {
+            HStack(spacing: 8) {
+                Button {
+                    withAnimation {
+                        showNewReminder.toggle()
+                    }
+                } label: {
+                    Constants.Images.addCircle
+                        .renderingMode(.template)
+                        .foregroundStyle(showNewReminder ? Constants.Colors.gray01 : Constants.Colors.black)
+
+                    Text("New Reminder")
+                        .foregroundStyle(showNewReminder ? Constants.Colors.gray01 : Constants.Colors.black)
+                        .font(Constants.Fonts.f2)
+                }
+                .disabled(showNewReminder)
+
+                Spacer()
+            }
+
+            showNewReminder ? createReminderCard : nil
+        }
+    }
+
+    private var createReminderCard: some View {
+        VStack(spacing: 24) {
+            VStack(spacing: 28) {
+                VStack(spacing: 16) {
+                    reminderDays
+
+                    HStack(spacing: 12) {
+                        Button {
+                            withAnimation {
+                                isEveryDay.toggle()
+                            }
+                        } label: {
+                            isEveryDay ? Constants.Images.checkboxFilled : Constants.Images.checkboxEmpty
+                        }
+
+                        Text("Every Day")
+                            .foregroundStyle(Constants.Colors.gray04)
+                            .font(Constants.Fonts.bodySemibold)
+
+                        Spacer()
+                    }
+                }
+
+                setTime
+            }
+            .padding(20)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Constants.Colors.white)
+            )
+            .upliftShadow(Constants.Shadows.smallLight)
+
+            HStack(spacing: 8) {
+                Spacer()
+
+                Button {
+                    isEveryDay = false
+                    isSettingTime = false
+                    withAnimation {
+                        showNewReminder.toggle()
+                    }
+                } label: {
+                    HStack(spacing: 8) {
+                        Constants.Images.trash
+
+                        Text("Delete")
+                            .foregroundStyle(Constants.Colors.black)
+                            .font(Constants.Fonts.h3)
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                    .background(Constants.Colors.white)
+                }
+                .cornerRadius(38)
+                .upliftShadow(Constants.Shadows.smallLight)
+
+                Button {
+                    // TODO: Implement adding a reminder
+                } label: {
+                    HStack(spacing: 8) {
+                        Constants.Images.check
+
+                        Text("Done")
+                            .foregroundStyle(Constants.Colors.white)
+                            .font(Constants.Fonts.h3)
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                    .background(Constants.Colors.black)
+                }
+                .cornerRadius(38)
+                .upliftShadow(Constants.Shadows.smallLight)
+
+                Spacer()
+            }
+        }
+    }
+
+    private var reminderDays: some View {
+        HStack(spacing: 20) {
+            ForEach(DayOfWeek.sortedDaysOfWeek(start: .monday), id: \.self) { day in
+                Text(day.dayOfWeekAbbreviation())
+                    .frame(width: 32, height: 32)
+                    .background {
+                        Circle()
+                            .fill(viewModel.selectedDays.contains(day)
+                                  ? Constants.Colors.yellow
+                                  : Constants.Colors.gray00)
+                    }
+                    .onTapGesture {
+                        withAnimation {
+                            viewModel.selectedDays.contains(day)
+                                ? viewModel.selectedDays.removeAll { $0 == day }
+                                : viewModel.selectedDays.append(day)
+                        }
+                    }
+            }
+        }
+        .font(Constants.Fonts.h3)
+        .foregroundStyle(Constants.Colors.black)
+        .padding(.vertical, 4)
+    }
+
+    private var setTime: some View {
+        VStack(spacing: 28) {
+            HStack(spacing: 16) {
+                Text("Set Time")
+                    .foregroundStyle(Constants.Colors.gray04)
+                    .font(Constants.Fonts.h3)
+
+                Toggle("", isOn: $isSettingTime.animation())
+                    .tint(Constants.Colors.yellow)
+                    .labelsHidden()
+
+                !isSettingTime ? (
+                    Text("Default is 9:00 am")
+                        .foregroundStyle(Constants.Colors.gray04)
+                        .font(Constants.Fonts.labelNormal)
+                ) : nil
+
+                Spacer()
+            }
+
+            isSettingTime ? (
+                VStack {
+                    // TODO: Choose time
+                }
+            ) : nil
+        }
+    }
+}
+
+#Preview {
+    SetGoalsView()
+}
