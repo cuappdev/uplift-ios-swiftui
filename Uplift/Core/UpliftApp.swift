ce687119309f70a844e6e91bb018f38176d8261b
Caitlyn Jin <cj384@cornell.edu>
2024-10-24 22:02:34 -0400

    Progress for messaging setup

--- Uplift/Core/UpliftApp.swift
+++ Uplift/Core/UpliftApp.swift
@@ -7,6 +7,8 @@
 //
 
 import FirebaseCore
+import FirebaseMessaging
+import FirebaseInstallations
 import SwiftUI
 
 @main
@@ -31,12 +33,47 @@ struct UpliftApp: App {
 
 }
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data,
+        didReceiveRegistrationToken fcmToken: String?
     ) -> Bool {
+        application.registerForRemoteNotifications()
         FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+
+        Task {
+            do {
+                let id = try await Installations.installations().installationID()
+                print("Installation ID: \(id)")
+            } catch {
+                print("Error fetching id: \(error)")
+            }
+        }
+
+        Messaging.messaging().apnsToken = deviceToken
+        print("Device token: \(deviceToken)")
+
+        if let fcm = Messaging.messaging().fcmToken {
+            print("FCM", fcm)
+        }
+
         return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        print("Device token: \(deviceToken)")
+    }
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcm = Messaging.messaging().fcmToken {
+            print("FCM", fcm)
+        } else {
+            print("FCM is nil")
+        }
     }
 }
