49f3e05f5896382fea27c9fa8d083a5d8274196e
Caitlyn Jin <cj384@cornell.edu>
2024-10-26 00:36:20 -0400

    Added report and user models (WIP)

---
+++ Uplift/Models/Report.swift
@@ -0,0 +1,50 @@
+//
+//  Report.swift
+//  Uplift
+//
+//  Created by Caitlyn Jin on 10/25/24.
+//  Copyright © 2024 Cornell AppDev. All rights reserved.
+//
+
+import Foundation
+import UpliftAPI
+
+/// Model representing a report.
+struct Report: Hashable {
+
+    // MARK: - Properties
+
+    /// The time at which this report was created.
+    let createdAt: DateTime
+
+    /// The desscription of this report.
+    let description: String
+
+    /// The gym this report is associated with.
+    let gym: Gym?
+
+    /// The ID of the gym in which this report is associated with.
+    let gymId: Int
+
+    /// The issue for this report.
+    let issue: ReportType
+
+    /// The user this report is associated with.
+    let user: User?
+
+    /// The ID of the user in which this report is associated with.
+    let userId: Int
+
+    // MARK: - Functions
+
+//    /// Initializes this object given an `EquipmentFields` type.
+//    init(from report: ReportFields) {
+//        // Unwrap and convert GraphQL enum value to Swift enum value
+//        self.accessibility = equipment.accessibility?.value
+//        self.equipmentType = equipment.equipmentType.value
+//        self.facilityId = equipment.facilityId
+//        self.name = equipment.name
+//        self.quantity = equipment.quantity
+//    }
+
+}
