64809e5d603e17eed635a3412fc21cce515b3764
Caitlyn Jin <cj384@cornell.edu>
2024-10-26 00:36:20 -0400

    Added report and user models (WIP)

---
+++ Uplift/Models/User.swift
@@ -0,0 +1,44 @@
+//
+//  User.swift
+//  Uplift
+//
+//  Created by Caitlyn Jin on 10/25/24.
+//  Copyright © 2024 Cornell AppDev. All rights reserved.
+//
+
+import Foundation
+import UpliftAPI
+
+/// Model representing a facility's equipment.
+struct User: Hashable {
+
+    // MARK: - Properties
+
+//    /// Accessibility for this equipment.
+//    let accessibility: AccessibilityType?
+//
+//    /// The type of this equipment.
+//    let equipmentType: EquipmentType?
+//
+//    /// The ID of the facility in which this equipment belongs to.
+//    let facilityId: Int
+//
+//    /// The name of this equipment.
+//    let name: String
+//
+//    /// The amount of this equipment in the given facility,`nil` if it cannot be quantified.
+//    let quantity: Int?
+//
+//    // MARK: - Functions
+//
+//    /// Initializes this object given an `EquipmentFields` type.
+//    init(from equipment: EquipmentFields) {
+//        // Unwrap and convert GraphQL enum value to Swift enum value
+//        self.accessibility = equipment.accessibility?.value
+//        self.equipmentType = equipment.equipmentType.value
+//        self.facilityId = equipment.facilityId
+//        self.name = equipment.name
+//        self.quantity = equipment.quantity
+//    }
+
+}
