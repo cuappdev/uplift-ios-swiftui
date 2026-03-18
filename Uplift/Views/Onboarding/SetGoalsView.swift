14603ece209bd263aaba6b38a44430bbd09f5e43
Caitlyn Jin <cj384@cornell.edu>
2026-03-18 16:48:24 -0400

    Add new UI

--- Uplift/Views/Onboarding/SetGoalsView.swift
+++ Uplift/Views/Onboarding/SetGoalsView.swift
@@ -13,7 +13,10 @@ struct SetGoalsView: View {
 
     // MARK: - Properties
 
+    let isOnboarding: Bool
+
     @EnvironmentObject var mainViewModel: MainView.ViewModel
+    @Environment(\.dismiss) private var dismiss
     @StateObject private var viewModel = ViewModel()
     @State private var isEveryDay = false
     @State private var isSettingTime = false
@@ -23,12 +26,37 @@ struct SetGoalsView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                header
-                content
+            if isOnboarding {
+                // TODO: Fix styling of header after workout history is pushed
+                VStack {
+                    header
+                    content
+                }
+                .padding(.vertical, 20)
+                .background(Constants.Colors.white)
+            } else {
+                VStack {
+                    headerInProfile
+                    content
+                }
+                .ignoresSafeArea(.all, edges: .top)
+                .navigationBarBackButtonHidden(true)
+                .toolbarBackground(.hidden, for: .navigationBar)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Constants.Images.arrowLeft
+                                .resizable()
+                                .scaledToFill()
+                                .foregroundStyle(Constants.Colors.black)
+                                .frame(width: 24, height: 24)
+                        }
+                    }
+                }
+                .background(Constants.Colors.white)
             }
-            .padding(.vertical, 20)
-            .background(Constants.Colors.white)
         }
     }
 
@@ -47,24 +75,42 @@ struct SetGoalsView: View {
         }
     }
 
+    private var headerInProfile: some View {
+        VStack {
+            Spacer()
+
+            HStack {
+                Spacer()
+
+                Text("Goals")
+                    .foregroundStyle(Constants.Colors.black)
+                    .font(Constants.Fonts.h2)
+
+                Spacer()
+            }
+        }
+        .padding(.bottom, 8)
+        .background(Constants.Colors.lightGray)
+        .frame(height: 96)
+    }
+
     private var content: some View {
-        VStack(spacing: 48) {
+        VStack(spacing: 40) {
             workoutDays
             // TODO: Workout reminders feature is archived for now
 //                workoutReminders
 
+            if !isOnboarding {
+                // TODO: Add save button
+                saveButton
+            }
+
             Spacer()
 
-            nextLabel
+            if isOnboarding {
+                nextButton
+            }
         }
-        .padding(
-            EdgeInsets(
-                top: Constants.Padding.goalsVertical,
-                leading: Constants.Padding.goalsHorizontal,
-                bottom: Constants.Padding.goalsVertical,
-                trailing: Constants.Padding.goalsHorizontal
-            )
-        )
     }
 
     private var workoutDays: some View {
@@ -98,6 +144,14 @@ struct SetGoalsView: View {
                 .padding(.horizontal, 10)
             }
         }
+        .padding(
+            EdgeInsets(
+                top: Constants.Padding.goalsVertical,
+                leading: Constants.Padding.goalsHorizontal,
+                bottom: Constants.Padding.goalsVertical,
+                trailing: Constants.Padding.goalsHorizontal
+            )
+        )
     }
 
     private var workoutReminders: some View {
@@ -163,7 +217,24 @@ struct SetGoalsView: View {
         }
     }
 
-    private var nextLabel: some View {
+    private var saveButton: some View {
+        Button {
+            withAnimation {
+
+            }
+        } label: {
+            Text("Save Changes")
+                .font(Constants.Fonts.h3)
+                .foregroundColor(Constants.Colors.white)
+                .padding(.horizontal, 32)
+                .padding(.vertical, 12)
+                .background(Constants.Colors.gray03)
+                .cornerRadius(30)
+                .upliftShadow(Constants.Shadows.smallLight)
+        }
+    }
+
+    private var nextButton: some View {
         Button {
             withAnimation {
                 mainViewModel.createUser {
@@ -191,5 +262,5 @@ struct SetGoalsView: View {
 }
 
 #Preview {
-    SetGoalsView()
+    SetGoalsView(isOnboarding: true)
 }
