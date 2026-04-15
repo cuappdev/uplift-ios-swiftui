3c583034da4962954acf77e94a13ead16790056f
Caitlyn Jin <cj384@cornell.edu>
2026-04-15 14:31:47 -0400

    Delete dummy workouts

--- Uplift/Utils/DummyData.swift
+++ Uplift/Utils/DummyData.swift
@@ -710,17 +710,6 @@ struct DummyData {
         ]
     ]
 
-    // TODO: Out of date dummy workouts
-//    static let dummyWorkouts: [Workout] = [
-//        Workout(id: "workout1", userId: 1, workoutTime: "2024-03-29T18:30:00Z", facilityId: 101, gymName: "Helen Newman"),
-//        Workout(id: "workout2", userId: 1, workoutTime: "2024-03-28T19:15:00Z", facilityId: 102, gymName: "Teagle Up"),
-//        Workout(id: "workout3", userId: 1, workoutTime: "2024-03-26T18:32:00Z", facilityId: 101, gymName: "Helen Newman"),
-//        Workout(id: "workout4", userId: 1, workoutTime: "2024-03-24T19:37:00Z", facilityId: 103, gymName: "Toni Morrison"),
-//        Workout(id: "workout5", userId: 1, workoutTime: "2024-03-23T10:02:00Z", facilityId: 101, gymName: "Helen Newman"),
-//        Workout(id: "workout6", userId: 1, workoutTime: "2024-03-20T17:45:00Z", facilityId: 102, gymName: "Teagle Up"),
-//        Workout(id: "workout7", userId: 1, workoutTime: "2024-03-18T08:15:00Z", facilityId: 101, gymName: "Helen Newman")
-//    ]
-
     static let dummyUser = User(
         id: "user1",
         email: "jj775@cornell.edu",
