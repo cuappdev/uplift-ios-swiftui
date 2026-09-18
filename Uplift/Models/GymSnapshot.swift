b7848356b702f01a42bc7fc6345bef65268081bb
Anatoli Monsalve <anatolim2007@gmail.com>
2026-09-18 15:18:18 -0400

    chore: clean up GymSnapshot

--- Uplift/Models/GymSnapshot.swift
+++ Uplift/Models/GymSnapshot.swift
@@ -10,21 +10,23 @@ import Foundation
 
 /// One block of fitness center open hours.
 struct HoursSnapshot: Codable, Hashable {
+
     // MARK: - Properties
-    let startTime: Date
+
     let endTime: Date
+    let startTime: Date
 
     // MARK: - Functions
 
-    ///Initializes`HoursSnapshot` with a `OpenHours` type
+    /// Initializes `HoursSnapshot` with an `OpenHours` type
     init(from hours: OpenHours) {
         self.endTime = hours.endTime
         self.startTime = hours.startTime
     }
 
     init(startTime: Date, endTime: Date) {
-            self.endTime = endTime
-            self.startTime = startTime
+        self.endTime = endTime
+        self.startTime = startTime
     }
 }
 
@@ -33,38 +35,38 @@ struct GymSnapshot: Codable, Hashable {
 
     // MARK: - Properties
 
+    let fitnessCenterHours: [HoursSnapshot]
     let id: String
-    let name: String
     let latitude: Double
     let longitude: Double
-    let fitnessCenterHours: [HoursSnapshot]
+    let name: String
 
     // MARK: - Functions
 
-    /// Initializes`GymSnapshot` with a `Gym` type
+    /// Initializes `GymSnapshot` with a `Gym` type
     init(from gym: Gym) {
+        self.fitnessCenterHours = gym.fitnessCenters.flatMap(\.hours).map(HoursSnapshot.init(from:))
         self.id = gym.id
-        self.name = gym.name
         self.latitude = gym.latitude
         self.longitude = gym.longitude
-        self.fitnessCenterHours = gym.fitnessCenters.flatMap(\.hours).map(HoursSnapshot.init(from:))
+        self.name = gym.name
     }
 
     init(id: String, name: String, latitude: Double, longitude: Double, fitnessCenterHours: [HoursSnapshot]) {
-            self.id = id
-            self.latitude = latitude
-            self.longitude = longitude
-            self.name = name
-            self.fitnessCenterHours = fitnessCenterHours
-    }
-
-    func isFitnessCenterOpen(at date: Date = Date()) -> Bool {
-        fitnessCenterHours.contains { $0.startTime <= date && date < $0.endTime }
+        self.fitnessCenterHours = fitnessCenterHours
+        self.id = id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.name = name
     }
 
     /// True when every saved hours block has already ended,
     /// meaning that this snapshot is too old to answer whether the gym is open.
     func hoursAreStale(at date: Date = Date()) -> Bool {
         fitnessCenterHours.allSatisfy { $0.endTime < date }
+    }
+
+    func isFitnessCenterOpen(at date: Date = Date()) -> Bool {
+        fitnessCenterHours.contains { $0.startTime <= date && date < $0.endTime }
     }
 }
